43e9e4757262c77fe539015a8c756c7c1fc290a2
minhtud04 <minhqnguyen_2026@depauw.edu>
2025-09-18 11:46:12 -0400

    Initialization

---
+++ src/main/java/depauw/mnguyen/delink/DelinkApplication.java
@@ -0,0 +1,13 @@
+package depauw.mnguyen.delink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depauw/mnguyen/delink/DelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package depauw.mnguyen.delink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
